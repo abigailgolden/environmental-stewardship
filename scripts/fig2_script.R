008211f3ab27869d89830e14b897cf0b8f6ca14d
abigailgolden <49074759+abigailgolden@users.noreply.github.com>
2024-08-01 11:21:49 -0700

    created figure visualizing patterns in responses to non-consensus questions by individuals

---
+++ scripts/fig2_script.R
@@ -0,0 +1,80 @@
+# visualize responses to controversial questions by individual
+# Setup -------------------------------------------------------------------
+
+
+rm(list = ls())
+options(scipen = 999)
+library(tidyverse)
+library(here)
+library(insight)
+library(gridExtra)
+library(lubridate)
+library(patchwork)
+
+datadir <- here::here("data")
+figdir <- here::here("figures")
+
+raw <- read.csv(paste(datadir, "policy-delphi-data.csv", sep = "/"), 
+                header = TRUE, as.is = TRUE, na.strings = "")
+
+# lower column names
+colnames(raw) <- tolower(colnames(raw))
+
+# save question text separately
+q_text <- unname(unlist(raw[1,]))
+
+# remove extra header rows
+
+dat1 <- tail(raw, -2)
+
+# extract multiple choice questions ---------------------------------------
+
+mc_q_text_key <- data.frame(question = q_text[c(19, 21, 23, 25, 27, 28, 30, 32)],
+                            question_id = c("q3", "q7", "q10", "q13", "q16", "q17", "q20", "q22"))
+
+
+mc_dat <- dat1 |> 
+  select(responseid, q3, q7, q10, q13) |> 
+  pivot_longer(cols = 2:5, values_to = "response", names_to = "question_id") |> 
+  left_join(mc_q_text_key, by = "question_id") |> 
+  mutate(response = ifelse(response == "I don't have enough information to answer this question",
+                           "I don't have enough information\nto answer this question",
+                           ifelse(response == "Anyone who takes an environmentally conscious action is acting as a steward, regardless of the outcome of that action",
+                                  "Taking action",
+                                  ifelse(response == "Only someone whose actions have positive environmental outcomes can be considered to be an environmental steward",
+                                         "Having positive outcomes",
+                                         ifelse(response == "Both", "Both organisms and ecosystems", response))))) |> 
+  mutate(response = factor(response, 
+                           levels = c("Yes", "No",
+                                      "Taking action",
+                                      "Having positive outcomes",
+                                      "I don't have enough information\nto answer this question")),
+         question_id = factor(question_id,
+                              levels = c("q3", "q7", "q10", "q13")))
+
+question_labs <- c("Non-interaction\nas stewardship",
+                   "Stewardship\nwithout care",
+                   "Stewardship\nwithout agency",
+                   "Action vs outcome")
+
+figname <- "Figure 2.png"
+png(paste(figdir, figname, sep = "/"), width = 12, height = 7, units = "in", res = 1000)
+
+
+ggplot(mc_dat, aes(x = question_id, y = responseid, fill = response))+
+  geom_tile()+
+  scale_x_discrete(labels = question_labs)+
+  scale_y_discrete(labels = rev(LETTERS[1:11]))+
+  labs(x = "Question Theme", 
+       y = "Respondent")+
+  scale_fill_manual(name = "Response",
+                    values = c("#4F7CBAFF", "#30BCADFF","#57A337", "#98DF8A","#A26DC2FF"), drop = FALSE
+  )+
+  theme_classic()+
+  theme(axis.text = element_text(size = 14),
+        axis.title = element_text(size = 17),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 17))
+
+dev.off()
+graphics.off()
